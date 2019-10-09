afb0310e61dc94571a9ba53ecfee6703d4fb9ed4
Laura <lfiscal@gmail.com>
2019-10-09 11:54:12 -0500

    Update cctweak.js

--- cctweak.js
+++ cctweak.js
@@ -1,36 +1,26 @@
 if (counter) throw counter.init(), "resetting";
 
 var counter = {
-        docObj: $("<span contenteditable='true' tabindex='-1'>0</span"),
+        docObj: $("<span contenteditable='true' tabindex='-1' style='padding: 6px 8px;border-radius:5px;border:1px solid lightgray; margin: 0px 10px;'>0</span"),
         projectName: $("#project-name").text(),
         skip: !0,
         id: "",
         init: function() {
-            this.docObj.insertBefore("#skip").css("padding", "9px"), localStorage.getItem(this.projectName) ? this.docObj.text(localStorage.getItem(this.projectName)) : localStorage.setItem(this.projectName, this.docObj.text()), this.onload()
+            this.docObj.insertBefore("#skip"), localStorage.getItem(this.projectName) ? this.docObj.text(localStorage.getItem(this.projectName)) : localStorage.setItem(this.projectName, this.docObj.text()), this.onload()
         },
         incr: function() {
             this.docObj.text(String(parseInt(this.docObj.text()) + 1)), localStorage.setItem(this.projectName, this.docObj.text())
         },
         onload: function() {
-            var e = $(".audio-id").text();
-            if (e !== this.id && e) {
-                if (this.id = e, $(".request-group").click(function() {
-                        this.skip = !0
-                    }), $(".request.active").hasClass("gradable") || !$(".request-group").length) {
-                    var t = !!$(".ui.positive.message").length;
-                    this.skip || t || this.incr(), this.skip = t
-                }
-                $(".ui.positive.message").length || setTimeout(function() {
-                    $(".ui.checkbox.slider input").click()
-                }, 100)
-            }
+            this.incr()
         }
     },
     s_ajaxListener = {
         tempOpen: XMLHttpRequest.prototype.open,
         tempSend: XMLHttpRequest.prototype.send,
         callback: function() {
-            this.url.includes("grade") && counter.onload()
+          console.log(this.url)
+          this.url.includes("grade") && counter.onload()
         }
     };
 XMLHttpRequest.prototype.open = function(e, t) {
